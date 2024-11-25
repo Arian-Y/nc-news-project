47a13f9377ab0ff45e02bb605e5eb93b6b8b0b4d
Arian-Y <arianyousefi04@gmail.com>
2024-11-25 15:49:19 +0000

    completed task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -53,3 +53,23 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: responds with the correct article when passed an article id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles.article_id).toBe(1);
+        expect(articles).toMatchObject({
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          body: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+        });
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
-const { getApi, getTopics } = require("./controllers/controllers");
+const { getApi, getTopics, getArticles } = require("./controllers/controllers");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
-app.all("*", (re, res) => {
+app.get("/api/articles/:article_id", getArticles);
+
+app.all("*", (re, res, next) => {
   res.status(404).send({ msg: "not found" });
+  next();
 });
 
+app.use((req, res) => {
+  res.status(500).send({ msg: "internal server error" });
+});
 module.exports = app;
