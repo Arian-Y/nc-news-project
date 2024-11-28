9e13fa0495fd898e911e91c52b50dbb9cee24de5
Arian-Y <arianyousefi04@gmail.com>
2024-11-28 10:02:03 +0000

    completed task 9

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -281,7 +281,7 @@ describe("PATCH /api/articles/:article_id", () => {
   });
 });
 
-describe.only("DELETE /api/comments/:comment_id", () => {
+describe("DELETE /api/comments/:comment_id", () => {
   test("204: returns an empty body when passed the comment_id", () => {
     return request(app)
       .delete("/api/comments/1")
@@ -303,6 +303,7 @@ describe.only("DELETE /api/comments/:comment_id", () => {
   test("404: return not found when passed a non existant article id", () => {
     return request(app)
       .delete("/api/comments/600")
+
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("not found");
