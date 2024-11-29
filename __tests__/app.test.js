4785da5eb5c2f4f2c17dbbce20158faed14ed45c
Arian-Y <arianyousefi04@gmail.com>
2024-11-29 13:28:06 +0000

    completed 11, 12 and 13

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,7 +56,7 @@ describe("GET /api/topics", () => {
   });
 });
 
-describe.only("GET /api/articles/:article_id", () => {
+describe("GET /api/articles/:article_id", () => {
   test("200: responds with the correct article when passed an article id", () => {
     return request(app)
       .get("/api/articles/1")
